2810937c8f0ba4b37431c0c2f77af39cb28a0730
Imran shaikh <imranpathan7040@gmail.com>
2024-01-15 20:08:47 +0530

    btn added

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const [count,setCount] = useState(0);
+
+  function increment(){
+    setCount(prev=> prev + 1);
+  }
+
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <p>Button clicked {count} times</p>
+        <button onClick={increment}>Click me</button>
     </div>
   )
 }
